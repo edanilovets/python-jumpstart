b5fcab7bb6964d2cd3e3b1cdacfa6abc29a7fa0f
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-07-10 14:48:15 +0300

    Add 04_journal_app

---
+++ 04_journal_app.py
@@ -0,0 +1,28 @@
+def print_header():
+    print("--------------------")
+    print("    JOURNAL APP")
+    print("--------------------")
+
+
+def run_event_loop():
+    print("What do you want to do with your journal?")
+    cmd = None
+
+    while cmd != "x":
+        cmd = input("[L]ist entries, [A]dd an entry, E[x]it: ")
+        if cmd.lower().strip() == "l":
+            print("L")
+        elif cmd.lower().strip() == "a":
+            print("A")
+        elif cmd.lower().strip() != "x":
+            print("Sorry, we don't understand '{}'".format(cmd))
+
+    print("Done, goodbye.")
+
+
+def main():
+    print_header()
+    run_event_loop()
+
+
+main()
