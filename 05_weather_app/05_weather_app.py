10f14214565b4d35c3f0adca4b3de80c1005dffd
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-07-12 11:31:11 +0300

    Finish 05_weather_app

--- 05_weather_app/05_weather_app.py
+++ 05_weather_app/05_weather_app.py
@@ -1,5 +1,8 @@
 import requests
 import bs4
+import collections
+
+WeatherReport = collections.namedtuple('WeatherReport', 'loc, temp')
 
 
 def main():
@@ -11,8 +14,10 @@ def main():
     # get html from web
     html = get_html_from_web(zip_code)
     # parse the html
-    get_weather_from_html(html)
+    report = get_weather_from_html(html)
     # display for the forecast
+    print('Location: {}'.format(report.loc))
+    print('Temperature: {}'.format(report.temp))
 
 
 def print_header():
@@ -38,13 +43,14 @@ def cleanup_text(text: str):
 
 
 def get_weather_from_html(html):
+
     soup = bs4.BeautifulSoup(html, "html.parser")
     loc = soup.find('h1').get_text()
     loc = cleanup_text(loc)
     temp = soup.find(class_='current-temp').get_text()
     temp = cleanup_text(temp)
-    # todo: continue lesson 50
-    print(loc, temp)
+    report = WeatherReport(loc=loc, temp=temp)
+    return report
 
 
 if __name__ == '__main__':
