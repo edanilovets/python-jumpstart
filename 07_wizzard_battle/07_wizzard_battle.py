f2f6e1877486ad501f5db4ae3940ed39d0a8b20b
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-07-13 15:11:03 +0300

    Finished 07_wizard_battle

--- 07_wizzard_battle/07_wizzard_battle.py
+++ 07_wizzard_battle/07_wizzard_battle.py
@@ -1,12 +1,17 @@
-from actors import Wizard, Creature
+import random
+import time
+
+from actors import Wizard, Creature, SmallAnimal, Dragon
 
 
 creatures = [
-    Creature('Toad', 1),
-    Creature('Bat', 3),
+    SmallAnimal('Toad', 1),
+    SmallAnimal('Bat', 3),
     Creature('Tiger', 12),
-    Creature('Dragon', 50),
-    Creature('Evil Wizard', 1000)
+    Dragon('Drogon', 30, 75, True),
+    Dragon('Viserion', 50, 25, False),
+    Dragon('Rhaegal', 30, 45, True),
+    Wizard('Evil Wizard', 1000)
 ]
 
 
@@ -28,15 +33,31 @@ def print_header():
 def game_loop():
 
     while True:
+        active_creature = random.choice(creatures)
+        print('\nA {} of level {} appeared from dark forest...\n'.format(
+            active_creature.name, active_creature.level
+        ))
+
         cmd = input('Do you [a]ttack, [r]un away, [l]ook around? ')
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print('The wizard runs and hides taking time to recover...%-)')
+                time.sleep(5)
+                print('The wizard returns full of power!')
         elif cmd == 'r':
-            print('runaway')
+            print('The wizard is a coward and runs away! Ha-ha-ha...')
         elif cmd == 'l':
-            print('look around')
+            print('The wizard {} looks around and sees: '.format(hero.name))
+            for c in creatures:
+                print('* {} of level {}'.format(c.name, c.level))
         else:
-            print('Exiting game!...')
+            print('Exiting game...')
+            break
+
+        if not creatures:
+            print('\nYou left alone, game over! Ha-ha-ha\nexiting game...\n')
             break
 
 
--- 07_wizzard_battle/actors.py
+++ 07_wizzard_battle/actors.py
@@ -1,9 +1,4 @@
-
-
-class Wizard:
-    def __init__(self, name, level):
-        self.name = name
-        self.level = level
+import random
 
 
 class Creature:
@@ -15,3 +10,62 @@ class Creature:
         return 'Creature: {} of level {}'.format(
             self.name, self.level
         )
+
+    def get_defensive_roll(self):
+        return random.randint(1, 12)*self.level
+
+
+class Wizard(Creature):
+
+    def attack(self, creature):
+        print('The wizard {} attacks {}'.format(
+            self.name, creature.name
+        ))
+
+        my_roll = self.get_defensive_roll()
+        creature_roll = creature.get_defensive_roll()
+
+        print('My roll: {}'.format(my_roll))
+        print('Creature {} rolls: {}'.format(creature.name, creature_roll))
+
+        if my_roll >= creature_roll:
+            print('You triumphed over {} :)'.format(creature.name))
+            return True
+        else:
+            print('You have been defeated! :(')
+            return False
+
+
+class SmallAnimal(Creature):
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        return base_roll / 20
+
+
+class Dragon(Creature):
+    def __init__(self, name, level, scaliness, breath_fire):
+        super().__init__(name, level)
+        self.scaliness = scaliness
+        self.breath_fire = breath_fire
+
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        fire_modifier = 5 if self.breath_fire else 1
+        scale_modifier = self.scaliness / 10
+        return base_roll * fire_modifier * scale_modifier
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
