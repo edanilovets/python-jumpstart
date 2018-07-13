fba1435cb965987efa7650dd5cba868527f57d10
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-07-13 17:54:35 +0300

    New app 08_file_searcher

---
+++ 08_file_searcher_app/08_file_searcher.py
@@ -0,0 +1,31 @@
+def main():
+    print_header()
+    folder = get_folder_from_user()
+    if not folder:
+        print('Invalid folder...')
+    text = get_search_text_from_user()
+    if not text:
+        print('Invalid text...')
+
+
+def print_header():
+    print('--------------------------')
+    print('       FILE SEARCHER')
+    print('--------------------------')
+    print()
+
+
+def get_folder_from_user():
+    pass
+
+
+def get_search_text_from_user():
+    pass
+
+
+def search_file():
+    pass
+
+
+if __name__ == '__main__':
+    main()
